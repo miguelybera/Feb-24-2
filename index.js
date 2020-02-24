17ab11ddca9bc1f6c13f0b2d12517fa65b1bb39c
CISCO <cisco@iicsadmin.com>
2020-02-24 12:58:01 +0800

    Third Commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function Person (name,gender,age){
-
-    this.name = name;
-    this.gender= gender;
-    this.age = age;
+class Person{
+    constructor(name, gender, age){
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+    walk(distance){
+        console.log(`${this.name} can walk up to ${distance} miles`)
+    }
 }
-
-let juan = new Person("Juan Dela Cruz", "Male", 35);
-document.write(`<h2>${juan.name} is a ${juan.gender} person </h2>`)
+let juan = new Person ("Juan Dela Cruz", "male", 34);
+juan.walk(5);
